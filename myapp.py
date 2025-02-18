dd37346f4a61c7d0f008dd1292989ccc989ae30c
wineguy-maker <lcbo@justemail.ca>
2025-02-18 12:51:51 -0500

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -330,7 +330,7 @@ def main():
             if pd.notna(thumbnail_url) and thumbnail_url != 'N/A':
                 detail_image_url = transform_image_url(thumbnail_url, "1280.1280.png")
                 st.image(detail_image_url, width=300)
-            if data['raw_lcbo_program'] != 'N/A': 
+            if row['raw_lcbo_program'] != 'N/A': 
                 st.markdown(f"**Title:** {row['raw_lcbo_program']}")
             st.markdown(f"**Title:** {row['title']}")
             st.markdown(f"**URL:** {row['uri']}")
